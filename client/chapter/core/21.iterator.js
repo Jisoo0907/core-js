38a12d8f75a58d86521a7b6d7edddd65eeeabb46
jisoolee <szjslee@gmail.com>
2024-06-17 11:43:39 +0900

    generator function

--- client/chapter/core/21.iterator.js
+++ client/chapter/core/21.iterator.js
@@ -54,10 +54,61 @@ a.next();
 a.next();
 a.next();
 
+/* -------------------------------------------------------------------------- */
+/*                             generator function                             */
+/* -------------------------------------------------------------------------- */
+function* gen() {
+  // function* 키워드 사용 -> 제너레이터 함수 정의
+  // 제너레이터 함수는 호출될 때 실행되지 않고, 대신 제너레이터 객체를 반환함
+  yield 1; // 제너레이터 함수가 실행을 일시 중단하고 1 반환
+  yield 2;
+  yield 3;
+}
+
+const gene = gen(); // gen 제너레이터 함수 호출, 제너레이터 객체 생성
+// gene 변수에 저장
+// 제너레이터 객체는 next 메서드를 사용하여 제너레이터 함수의 실행 제어
+console.log(gene.next()); // gene.next()호출 -> 제너레이터 함수의 실행 제어
+// 현재 위치에서 다음 yield 표현식까지 실행함
+// next 메서드는 {value:X, done:Y}형태의 객체 반환함
+// value는 yield 된 값, done은 제너레이터 함수가 끝났는지를 나타내는 불리언 값
+console.log(gene.next());
+console.log(gene.next());
+// 까지 하고 다음 부터는 undefined 나옴
+
+const customIter = {
+  *[Symbol.iterator]() {
+    yield 1;
+    yield 2;
+    yield 3;
+  },
+};
+
+for (const a of customIter) {
+  console.log(a);
+}
+
 // 일반 객체는 next를 호출할 수 없음
 
 // 유사배열 vs. 이터러블
 // - 유사배열 : 인덱스 키와 length 속성을 가진 객체
 // - 이터러블 : Symbol.Iterator 메서드를 가지는 객체
+
+function* idGenerator() {
+  // let id = 1;
+  while (true) {
+    yield `user-${crypto.randomUUID}`;
+  }
+}
+
+const id = idGenerator();
+
+// generator
+// 1. 일관된 반복 동작 제공 (for..of)
+// 2. 커스텀 반복 제어 가능 (객체를 반복 가능한 상태로)
+// 3. 지연 계산 (필요할 때 마다 반복을 돌림) 성능
+// 4. 무한 시퀀스 생성 (무한대의 값 생성)
+// 5. 비동기 반복 작업
+// 6. 다양한 데이터 소스와의 통합 ( Map, Set )
 
 // 유사배열, 이터러블 배열화
