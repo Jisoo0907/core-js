53ad0ca9dea858a6ec4ed3b38285d285367ed031
jisoolee <szjslee@gmail.com>
2024-06-19 11:34:35 +0900

    getStyle, setStyle 함수 lib/dom/css.js에 구현

--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -1,3 +1,6 @@
+/* -------------------------------------------------------------------------- */
+/*                                    class                                   */
+/* -------------------------------------------------------------------------- */
 function addClass(node, ...className) {
   if (typeof node === 'string') node = document.querySelector(node);
 
@@ -43,3 +46,38 @@ function toggleClass(node, className) {
 
   return node.classList.toggle(className);
 }
+
+/* -------------------------------------------------------------------------- */
+/*                                    style                                   */
+/* -------------------------------------------------------------------------- */
+function getStyle(node, prop) {
+  if (isString(node)) node = getNode(node);
+
+  if (!(prop in document.body.style)) {
+    throw new SyntaxError(
+      'getStyle 함수의 두 번째 인수는 유효한 css 속성이 아닙니다.'
+    );
+  }
+
+  return getComputedStyle(node)[prop];
+}
+
+function setStyle(node, prop, value) {
+  if (isString(node)) node = getNode(node);
+
+  if (!(prop in document.body.style)) {
+    throw new SyntaxError(
+      'setStyle 함수의 두 번째 인수는 유효한 css 속성이 아닙니다.'
+    );
+  }
+
+  if (!value)
+    throw new ReferenceError(
+      'setStyle 함수의 세 번째 인수는 필수 입력값 입니다.'
+    );
+
+  node.style[prop] = value;
+}
+
+const css = (node, prop, value) =>
+  !value ? getStyle(node, prop) : setStyle(node, prop, value);
