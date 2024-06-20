04566200fa168a030dfcbbf3adbe9b4bc61447c3
jisoolee <szjslee@gmail.com>
2024-06-20 13:48:11 +0900

    마크업 수정 후 validation 실습

--- client/chapter/dom/09.validation.js
+++ client/chapter/dom/09.validation.js
@@ -7,27 +7,28 @@ const nav = getNode('nav');
 function handleClick(e) {
   const target = e.target;
   const name = target.dataset.name;
+  const li = target.closest('li'); // ul위에는 li가 없으니까 ul 클릭 시 null
 
-  if (target.tagName !== 'LI') return; // 정확히 li만 조회하기
+  if (!li) return; // ul 거르기
 
   // console.log(e.target); // 여백 클릭하면 ul이 뜸
 
   /* 클래스를 사용한 위임 ---------------- */
-  // if (target.matches.contains('about')) {
+  // if (li.matches.contains('about')) {
   //   console.log('about!');
   // }
-  // if (target.classList.contains('about')) {
+  // if (li.classList.contains('about')) {
   //   console.log('home!');
   // }
-  // if (target.classList.contains('about')) {
+  // if (li.classList.contains('about')) {
   //   console.log('contact!');
   // }
   /*                                        */
 
   /* 속성을 사용한 위임 ------------------ */ // 범쌤 픽
-  // console.log(target.dataset.name); // 범쌤은 객체 안에서만 찾으니 빠를 거라고 생각
-  // console.log(target.getAttribute('data-name'));
-  // console.log(attr(target, 'data-name'));
+  // console.log(li.dataset.name); // 범쌤은 객체 안에서만 찾으니 빠를 거라고 생각
+  // console.log(li.getAttribute('data-name'));
+  // console.log(attr(li, 'data-name'));
 
   // if (name === 'about') console.log('about!');
   // if (name === 'home') console.log('home!');
@@ -37,9 +38,9 @@ function handleClick(e) {
   /* 노드를 사용한 위임 ------------------ */
   // console.log(target.textContent.includes('ABOUT'));
   // 위 방식은 ul 찍었을 때도 true 뜸
-  if (target.textContent.includes('ABOUT')) console.log('about!!');
-  if (target.textContent.includes('HOME')) console.log('home!!');
-  if (target.textContent.includes('CONTACT')) console.log('contact!!');
+  if (li.textContent.includes('ABOUT')) console.log('about!!');
+  if (li.textContent.includes('HOME')) console.log('home!!');
+  if (li.textContent.includes('CONTACT')) console.log('contact!!');
   /*                                      */
 }
 
