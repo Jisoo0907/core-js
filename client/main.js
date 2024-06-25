a6f63acf1f77c20e25110e1ae2bcd20a07e63a9b
jisoolee <szjslee@gmail.com>
2024-06-25 11:38:01 +0900

    complete randomDice - renderItem - createItem - resetItem

--- client/main.js
+++ client/main.js
@@ -1,23 +1,49 @@
 import {
-  diceAnimation,
+  attr,
   getNode,
   getNodes,
-  attr,
   insertLast,
+  diceAnimation,
+  endScroll,
+  clearContents,
 } from './lib/index.js';
 
 // 1. 주사위 애니메이션
-// 2. 주사위 굴리기 버튼 클릭 => diceAnimation() 실행될 수 있도록
-
-// const buttonGroups = getNodes('.buttonGroup > button');
-/* const rollingButton = buttonGroups[0];
-const recordButton = buttonGroups[1];
-const resetButton = buttonGroups[2]; */
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
 
 const [rollingButton, recordButton, resetButton] = getNodes(
   '.buttonGroup > button'
 );
 const recordListWrapper = getNode('.recordListWrapper');
+
+let count = 0;
+let total = 0;
+
+function createItem(value) {
+  const template = `
+    <tr>
+      <td>${++count}</td>
+      <td>${value}</td>
+      <td>${(total += value)}</td>
+    </tr>
+  `;
+  return template;
+}
+
+function renderRecordItem() {
+  // const diceValue = getNode('#cube').getAttribute('dice');
+  const diceValue = Number(attr(getNode('#cube'), 'dice'));
+
+  insertLast('.recordList tbody', createItem(diceValue));
+
+  // 1. insertLast 함수 사용
+
+  // 2. template 전달
+
+  // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
+
+  endScroll(recordListWrapper);
+}
 
 const handleRollingDice = (() => {
   let isClicked = false;
@@ -33,40 +59,20 @@ const handleRollingDice = (() => {
       recordButton.disabled = false;
       resetButton.disabled = false;
     }
-
     isClicked = !isClicked;
   };
 })();
 
-let count = 0;
-let total = 0;
-
-// 함수는 하나의 일만 처리
-function renderRecordItem() {
-  // const diceValue = getNode('#cube').getAttribute('dice');
-  const diceValue = attr(getNode('#cube'), 'dice') * 1; // Number() 하든지ㅂ
-  const template = `
-            <tr>
-              <td>${++count}</td>
-              <td>${diceValue}</td>
-              <td>${(total += diceValue)}</td>
-            </tr>`; // 얘를 렌더링 -(2)
-  /* 
-1. insertList 함수 사용 || element.insertAdjacentHTML
-2. template 전달
-3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
-*/
-  insertLast('.recordList tbody', template); // 렌더링 뭐를? -(1)
-}
-
 function handleRecord() {
   recordListWrapper.hidden = false;
-
   renderRecordItem();
 }
 
 function handleReset() {
   recordListWrapper.hidden = true;
+  clearContents('tbody');
+  count = 0;
+  total = 0;
 }
 
 rollingButton.addEventListener('click', handleRollingDice);
