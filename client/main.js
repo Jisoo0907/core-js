97a4cf611156c29ce0c4860b1e1c2479a0ed8bc5
jisoolee <szjslee@gmail.com>
2024-07-03 11:39:04 +0900

    template

--- client/main.js
+++ client/main.js
@@ -1,23 +1,17 @@
-class Button extends HTMLElement {
-  constructor() {
-    super();
+/* const template = document.createElement('template');
 
-    this.attachShadow({ mode: 'open' }); // c-button의 쉐도우 돔을 열어줘
-    this.shadowRoot.innerHTML = ` 
-    <button>hello</button>
-    `;
-    // console.log(this.shadowRoot); // 위에서 close하면 안 나옴
-  }
+template.innerHTML = `
+<div>bye</div>
+<div>javascript</div>
+`;
 
-  connectedCallback() {}
+console.log(template);
+ */
+const app = document.querySelector('#app');
+const temp = document.querySelector('#temp');
 
-  disconnectedCallback() {}
-}
+const clone = temp.content.cloneNode(true);
 
-customElements.define('c-button', Button); // button태그를 c-button으로 확장시키겠다
+// console.log(temp.content); // temp.content: document-fragment
 
-// 밖에서 수집이 될까?
-console.log(document.querySelector('button')); // null
-// 접근 자체가 막혀 있음
-// 쉐도우 루트를 직접 잡고 접근해야 함
-console.log(document.querySelector('c-button').shadowRoot);
+app.appendChild(clone);
