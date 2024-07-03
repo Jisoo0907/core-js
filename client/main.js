91016fdbec356cd26d9d49663f48626d494f78c5
jisoolee <szjslee@gmail.com>
2024-07-03 13:46:29 +0900

    event binding

--- client/main.js
+++ client/main.js
@@ -3,23 +3,29 @@ class UserCard extends HTMLElement {
     super();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
-       <style>
-        :host{
-          background-color: orange;
-        }
-        div{
-          background:red;
-        }
-      </style>
-        <div> nickName : kind-tiger </div>
-        <slot name="username"></slot>
-        <slot name="age"></slot>
-        <slot name="gender"></slot>
-        <slot name="common"></slot>
-        <slot></slot>
-         <slot name="email">any@naver.com</slot>
+     <button type="button">btn</button>
     `;
+
+    this.button = this.shadowRoot.querySelector('button');
+    console.log(this.button);
+  }
+
+  connectedCallback() {
+    // DOM 조작할 거면 이 안에 넣는 게 좋음
+    this.button.addEventListener('click', () => this.clickMe());
+  }
+
+  clickMe() {
+    console.log(this);
   }
 }
 
 customElements.define('user-card', UserCard);
+// console.log( document.querySelector('user-card').shadowRoot.querySelector('button') );
+
+// sum() // undefined
+// sum.call({}) // {} => 실행
+// sum.apply({}) // {} => 실행
+// const n = sum.bind({}) // {} => 실행 x
+
+// n()
